9c0e5c97b67774800286bd8f6dc05e22f075d492
iui <iui@iui.com>
2021-05-31 01:39:55 +0800

    坐标转换及折腾Pangolin m1 build

--- ch3/examples/coordinateTransform.cpp
+++ ch3/examples/coordinateTransform.cpp
@@ -7,18 +7,22 @@
 using namespace std;
 using namespace Eigen;
 
-int main(int argc, char** argv) {
-  Quaterniond q1(0.35, 0.2, 0.3, 0.1), q2(-0.5, 0.4, -0.1, 0.2);
-  q1.normalize();
-  q2.normalize();
-  Vector3d t1(0.3, 0.1, 0.1), t2(-0.1, 0.5, 0.3);
-  Vector3d p1(0.5, 0, 0.2);
+int main(int argc, char **argv) {
+    //小萝卜头1号，2号，的旋转用四元数表示
+    Quaterniond q1(0.35, 0.2, 0.3, 0.1), q2(-0.5, 0.4, -0.1, 0.2);
+    q1.normalize();
+    q2.normalize();
+    //小萝卜头1号，2号，的平移用向量表示
+    Vector3d t1(0.3, 0.1, 0.1), t2(-0.1, 0.5, 0.3);
+    //1号看到某一个点在自身坐标系下的坐标
+    Vector3d p1(0.5, 0, 0.2);
 
-  Isometry3d T1w(q1), T2w(q2);
-  T1w.pretranslate(t1);
-  T2w.pretranslate(t2);
+    Isometry3d T1w(q1), T2w(q2);
+    T1w.pretranslate(t1);
+    T2w.pretranslate(t2);
 
-  Vector3d p2 = T2w * T1w.inverse() * p1;
-  cout << endl << p2.transpose() << endl;
-  return 0;
+    //T1w.inverse()*p1先转换到世界坐标，然后左成2号的变换矩阵，就是在2号坐标系下的坐标。
+    Vector3d p2 = T2w * T1w.inverse() * p1;
+    cout << endl << p2.transpose() << endl;
+    return 0;
 }
